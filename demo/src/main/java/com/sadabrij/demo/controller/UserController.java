ac33f8ad034fd128c3ada7c114971c3fe6d94342
sadabrij-shah <shah.sadabrij24@gmail.com>
2024-08-11 00:04:06 +0530

    First commit

---
+++ demo/src/main/java/com/sadabrij/demo/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.sadabrij.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	
+	@GetMapping("/")
+	public ResponseEntity<String> getUserName(){
+		
+		return ResponseEntity.ok("Hello user");
+		
+	}
+
+}
